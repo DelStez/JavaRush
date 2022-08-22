0eef3b96dc62aedc73854c814d25ce608022fdab
Delstez <kandakova0002@gmail.com>
2022-08-23 02:00:41 +0500

    add cycle for set cell values

--- 8.Games/src/com/javarush/games/snake/Snake.java
+++ 8.Games/src/com/javarush/games/snake/Snake.java
@@ -2,15 +2,25 @@ package com.javarush.games.snake;
 
 import java.util.ArrayList;
 import java.util.List;
+import com.javarush.engine.cell.*;
 
 public class Snake {
     public int x;
     public int y;
-    private List<GameObject> snakeParts;
+    private final static String HEAD_SIGN = "\uD83D\uDC7E";
+    private final static String BODY_SIGN = "\u26AB";
+    private List<GameObject> snakeParts = new ArrayList<>();
+    private Snake snake;
     public Snake(int x, int y){
         this.snakeParts = new ArrayList<>();
         snakeParts.add(new GameObject(x, y));
         snakeParts.add(new GameObject(x + 1, y));
         snakeParts.add(new GameObject(x + 2, y));
+    }
+    public void draw(Game game) {
+        for(int j = 0; j < snakeParts.size(); j++) {
+            game.setCellValue(snakeParts.get(j).x, snakeParts.get(j).y,
+                    j==0 ? HEAD_SIGN :BODY_SIGN);
+        }
     }
 }
--- 8.Games/src/com/javarush/games/snake/SnakeGame.java
+++ 8.Games/src/com/javarush/games/snake/SnakeGame.java
@@ -5,6 +5,7 @@ import com.javarush.engine.cell.*;
 public class SnakeGame extends Game{
     public static final int WIDTH = 15;
     public static final int HEIGHT = 15;
+    private Snake snake;
 
     public void initialize(){
         setScreenSize(WIDTH, HEIGHT);
@@ -16,8 +17,10 @@ public class SnakeGame extends Game{
                 setCellColor(x, y, Color.SANDYBROWN);
             }
         }
+        snake.draw(SnakeGame.this);
     }
     private void createGame(){
+        snake = new Snake(WIDTH/2, HEIGHT/2);
         drawScene();
         //Apple apple = new Apple(7,7);
         //apple.draw(this);
